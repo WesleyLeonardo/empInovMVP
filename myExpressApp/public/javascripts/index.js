f08bb92a9ae9f084b0ab87d685891eb7c52e4e08
WesleyLeonardo <wesleyleonardo018@hotmail.com>
2023-07-01 20:16:25 -0400

    Simplificação dos dados devido ao tempo. Heatmap sem relação com dados reais. Dados gerados aleatóriamente.

--- myExpressApp/public/javascripts/index.js
+++ myExpressApp/public/javascripts/index.js
@@ -1,8 +1,8 @@
 // Função para inicializar o mapa
 function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
-        center: {lat: -14.235004, lng: -51.92528},
-        zoom: 4.5,
+        center: {lat: -20.4617189, lng: -54.61223709999999},
+        zoom: 12,
         mapTypeControl: true,
     });
 
@@ -48,60 +48,26 @@ function initMap() {
 
     //Dados do Heatmap
     var heatmapData = [
-        //Aracaju - Sergipe
-        {location: new google.maps.LatLng(-10.91, -37.07), weight: Math.random()*100},
-        //Belém - Pará
-        {location: new google.maps.LatLng(-1.46, -48.48), weight: Math.random()*100},
-        //Belo Horizonte - Minas Gerais
-        {location: new google.maps.LatLng(-19.92, -43.94), weight: Math.random()*100},
-        //Boa Vista - Roraima
-        {location: new google.maps.LatLng(2.82, -60.67), weight: Math.random()*100},
-        //Brasília
-        {location: new google.maps.LatLng(-15.78, -47.92), weight: Math.random()*100},
-        //Campo Grande
-        {location: new google.maps.LatLng(-20.481389, -54.616111), weight: Math.random()*100},
-        //Cuiabá - Mato Grosso
-        {location: new google.maps.LatLng(-15.6, -56.1), weight: Math.random()*100},
-        //Curitiba - Paraná
-        {location: new google.maps.LatLng(-25.42, -49.29), weight: Math.random()*100},
-        //Florianópolis - Santa Catarina
-        {location: new google.maps.LatLng(-27.59, -48.55), weight: Math.random()*100},
-        //Fortaleza - Ceará
-        {location: new google.maps.LatLng(-3.71, -38.54), weight: Math.random()*100},
-        //Goiânia - Goiás
-        {location: new google.maps.LatLng(-16.68, -49.25), weight: Math.random()*100},
-        //João Pessoa - Paraíba
-        {location: new google.maps.LatLng(-7.12, -34.86), weight: Math.random()*100},
-        //Macapá - Amapá
-        {location: new google.maps.LatLng(0.035, -51.07), weight: Math.random()*100},
-        //Maceió - Alagoas
-        {location: new google.maps.LatLng(-9.66, -35.73), weight: Math.random()*100},
-        //Manaus - Amazonas
-        {location: new google.maps.LatLng(-3.11, -60.02), weight: Math.random()*100},
-        //Natal - Rio Grande do Norte
-        {location: new google.maps.LatLng(-5.81, -35.21), weight: Math.random()*100},
-        //Palmas - Tocantins
-        {location: new google.maps.LatLng(-10.24, -48.35), weight: Math.random()*100},
-        //Porto Alegre - Rio Grande do Sul
-        {location: new google.maps.LatLng(-30.03, -51.23), weight: Math.random()*100},
-        //Porto Velho - Rondônia
-        {location: new google.maps.LatLng(-8.76, -63.9), weight: Math.random()*100},
-        //Recife - Pernambuco
-        {location: new google.maps.LatLng(-8.05, -34.9), weight: Math.random()*100},
-        //Rio Branco - Acre
-        {location: new google.maps.LatLng(-9.97, -67.81), weight: Math.random()*100},
-        //Rio de Janeiro
-        {location: new google.maps.LatLng(-22.91, -43.2), weight: Math.random()*100},
-        //Salvador - Bahia
-        {location: new google.maps.LatLng(-12.97, -38.51), weight: Math.random()*100},
-        //São Luís - Maranhão
-        {location: new google.maps.LatLng(-2.53, -44.3), weight: Math.random()*100},
-        //São Paulo
-        {location: new google.maps.LatLng(-23.55, -46.64), weight: Math.random()*100},
-        //Teresina - Piauí
-        {location: new google.maps.LatLng(-5.09, -42.8), weight: Math.random()*100},
-        //Vitória - Espírito Santo
-        {location: new google.maps.LatLng(-20.32, -40.34), weight: Math.random()*100},
+//Centro
+        {location: new google.maps.LatLng(-20.4617189, -54.61223709999999), weight: Math.random()*100},
+//Tiradentes
+        {location: new google.maps.LatLng(-20.4804084, -54.5711203), weight: Math.random()*100},
+//Vila Vilas Boas
+        {location: new google.maps.LatLng(-20.4874507, -54.59030560000001), weight: Math.random()*100},
+//Pioneiros
+        {location: new google.maps.LatLng(-20.5111925, -54.614979), weight: Math.random()*100},
+//Aero Rancho
+        {location: new google.maps.LatLng(-20.51886, -54.6478885), weight: Math.random()*100},
+//Jardim São Conrado
+        {location: new google.maps.LatLng(-20.4965941, -54.6792266), weight: Math.random()*100},
+//Vila Taveiropolis
+        {location: new google.maps.LatLng(-20.4750862, -54.6808111), weight: Math.random()*100},
+//Santa Fe
+        {location: new google.maps.LatLng(-20.4527743, -54.5900068), weight: Math.random()*100},
+//Coronel Antonino
+        {location: new google.maps.LatLng(-20.4206427, -54.59030560000001), weight: Math.random()*100},
+//São Francisco
+        {location: new google.maps.LatLng(-20.4462987, -54.61223709999999), weight: Math.random()*100},
     ];
 
     console.log(heatmapData);
@@ -160,6 +126,27 @@ function initMap() {
 
         if (place.geometry.viewport) {
             map.fitBounds(place.geometry.viewport);
+            //salva a latitude e longitude e o nome do bairro em um array e salva em um arquivo json
+            var lat = place.geometry.location.lat();
+            var lng = place.geometry.location.lng();
+            var name = place.name;
+
+            /*mostra uma caixa de texto as informações no formato:
+            "//nome do bairro"
+            {location: new google.maps.LatLng(lat, lng), weight: Math.random()*100},
+             */
+            var text = "//" + name + "\n" + "{location: new google.maps.LatLng(" + lat + ", " + lng + "), weight: Math.random()*100},";
+            //copia o texto para a área de transferência
+            navigator.clipboard.writeText(text);
+
+
+
+
+
+
+
+
+
         } else {
             map.setCenter(place.geometry.location);
             map.setZoom(10);
